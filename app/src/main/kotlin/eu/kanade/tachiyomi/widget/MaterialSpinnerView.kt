2819c52066b606494c7e3294367ed5a4dfe8e6ca
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-03-06 09:25:48 +0800

    Update MaterialSpinnerView.kt

--- app/src/main/kotlin/eu/kanade/tachiyomi/widget/MaterialSpinnerView.kt
+++ app/src/main/kotlin/eu/kanade/tachiyomi/widget/MaterialSpinnerView.kt
@@ -12,158 +12,130 @@ import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.appcompat.widget.PopupMenu
 import androidx.core.content.withStyledAttributes
-import androidx.core.view.forEach
-import androidx.core.view.get
-import androidx.core.view.size
-import com.hippo.ehviewer.R
-import com.hippo.ehviewer.databinding.PrefSpinnerBinding
+import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.databinding.PrefSpinnerBinding
 import eu.kanade.tachiyomi.core.preference.Preference
 import eu.kanade.tachiyomi.util.system.getResourceColor
 
-class MaterialSpinnerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
-    FrameLayout(context, attrs) {
+class MaterialSpinnerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : FrameLayout(context, attrs) {
 
-    private var entries = emptyList<String>()
+    private val entries: List<String>
     private var selectedPosition = 0
     private var popup: PopupMenu? = null
+
+    private val emptyIcon by lazy { AppCompatResources.getDrawable(context, R.drawable.ic_blank_24dp) }
+    private val checkmarkIcon by lazy { AppCompatResources.getDrawable(context, R.drawable.ic_check_24dp)?.mutate()?.apply { setTint(context.getResourceColor(android.R.attr.textColorPrimary)) } }
+
+    private val binding: PrefSpinnerBinding
 
     var onItemSelectedListener: ((Int) -> Unit)? = null
         set(value) {
             field = value
-            if (value != null) {
-                popup = makeSettingsPopup()
-                setOnTouchListener(popup?.dragToOpenListener)
-                setOnClickListener {
-                    popup?.show()
-                }
-            }
+            setupPopupMenu()
         }
-
-    private val emptyIcon by lazy {
-        AppCompatResources.getDrawable(context, R.drawable.ic_blank_24dp)
-    }
-    private val checkmarkIcon by lazy {
-        AppCompatResources.getDrawable(context, R.drawable.ic_check_24dp)?.mutate()?.apply {
-            setTint(context.getResourceColor(android.R.attr.textColorPrimary))
-        }
-    }
-
-    private val binding = PrefSpinnerBinding.inflate(LayoutInflater.from(context), this, false)
 
     init {
-        addView(binding.root)
+        binding = PrefSpinnerBinding.inflate(LayoutInflater.from(context), this, true)
+        context.withStyledAttributes(attrs, R.styleable.MaterialSpinnerView) {
+            binding.title.text = getString(R.styleable.MaterialSpinnerView_title).orEmpty()
+            entries = getTextArray(R.styleable.MaterialSpinnerView_android_entries)?.map(CharSequence::toString) ?: emptyList()
+            binding.details.text = entries.firstOrNull().orEmpty()
+        }
+        setupPopupMenu()
+    }
 
-        context.withStyledAttributes(set = attrs, attrs = R.styleable.MaterialSpinnerView) {
-            val title = getString(R.styleable.MaterialSpinnerView_title).orEmpty()
-            binding.title.text = title
+    private fun setupPopupMenu() {
+        popup?.dismiss()
+        popup = PopupMenu(context, this, Gravity.END, androidx.appcompat.R.attr.actionOverflowMenuStyle, 0)
+        popup?.let { popupMenu ->
+            entries.forEachIndexed { index, entry ->
+                popupMenu.menu.add(0, index, 0, entry).also {
+                    it.icon = emptyIcon
+                }
+            }
 
-            val viewEntries = (
-                getTextArray(R.styleable.MaterialSpinnerView_android_entries)
-                    ?: emptyArray()
-                ).map { it.toString() }
-            entries = viewEntries
-            binding.details.text = viewEntries.firstOrNull().orEmpty()
+            (popupMenu.menu as? MenuBuilder)?.setOptionalIconsVisible(true)
+
+            popupMenu.menu.forEach { it.icon = emptyIcon }
+            popupMenu.menu[selectedPosition].icon = checkmarkIcon
+
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                val pos = menuClicked(menuItem)
+                onItemSelectedListener?.invoke(pos)
+                true
+            }
+
+            setOnTouchListener(popupMenu.dragToOpenListener)
+            setOnClickListener {
+                popupMenu.show()
+            }
         }
     }
 
     fun setSelection(selection: Int) {
-        if (selectedPosition < (popup?.menu?.size ?: 0)) {
-            popup?.menu?.getItem(selectedPosition)?.let {
-                it.icon = emptyIcon
-            }
+        if (selectedPosition in 0 until (popup?.menu?.size() ?: 0)) {
+            popup?.menu?.getItem(selectedPosition)?.icon = emptyIcon
         }
         selectedPosition = selection
-        if (selectedPosition < (popup?.menu?.size ?: 0)) {
-            popup?.menu?.getItem(selectedPosition)?.let {
-                it.icon = checkmarkIcon
-            }
-        }
+        popup?.menu?.getItem(selection)?.icon = checkmarkIcon
         binding.details.text = entries.getOrNull(selection).orEmpty()
     }
 
     fun bindToPreference(pref: Preference<Int>, offset: Int = 0, block: ((Int) -> Unit)? = null) {
         setSelection(pref.get() - offset)
-
-        popup = makeSettingsPopup(pref, offset, block)
-        setOnTouchListener(popup?.dragToOpenListener)
-        setOnClickListener {
-            popup?.show()
+        setupPopupMenu {
+            pref.set(it + offset)
+            block?.invoke(it)
         }
     }
 
     fun <T : Enum<T>> bindToPreference(pref: Preference<T>, clazz: Class<T>) {
         val enumConstants = clazz.enumConstants
-        enumConstants?.indexOf(pref.get())?.let { setSelection(it) }
-
-        popup = makeSettingsPopup(pref, clazz)
-        setOnTouchListener(popup?.dragToOpenListener)
-        setOnClickListener {
-            popup?.show()
-        }
+        enumConstants?.indexOf(pref.get())?.let(this::setSelection)
+        setupPopupMenu(pref, clazz)
     }
 
     fun bindToIntPreference(pref: Preference<Int>, @ArrayRes intValuesResource: Int, block: ((Int) -> Unit)? = null) {
-        val intValues = resources.getStringArray(intValuesResource).map { it.toIntOrNull() }
-        setSelection(intValues.indexOf(pref.get()))
-
-        popup = makeSettingsPopup(pref, intValues, block)
-        setOnTouchListener(popup?.dragToOpenListener)
-        setOnClickListener {
-            popup?.show()
-        }
+        val intValues = resources.getStringArray(intValuesResource).mapNotNull(String::toIntOrNull)
+        intValues.indexOf(pref.get()).also(this::setSelection)
+        setupPopupMenu(pref, intValues, block)
     }
 
-    private fun <T : Enum<T>> makeSettingsPopup(preference: Preference<T>, clazz: Class<T>): PopupMenu {
-        return createPopupMenu { pos ->
-            onItemSelectedListener?.invoke(pos)
-
+    private fun <T : Enum<T>> setupPopupMenu(preference: Preference<T>, clazz: Class<T>) {
+        setupPopupMenu {
             val enumConstants = clazz.enumConstants
-            enumConstants?.get(pos)?.let { enumValue -> preference.set(enumValue) }
+            enumConstants?.get(it)?.let { enumValue -> preference.set(enumValue) }
         }
     }
 
-    private fun makeSettingsPopup(preference: Preference<Int>, intValues: List<Int?>, block: ((Int) -> Unit)? = null): PopupMenu {
-        return createPopupMenu { pos ->
-            preference.set(intValues[pos] ?: 0)
-            block?.invoke(pos)
+    private fun setupPopupMenu(preference: Preference<Int>, intValues: List<Int?>, block: ((Int) -> Unit)? = null) {
+        setupPopupMenu {
+            preference.set(intValues[it] ?: 0)
+            block?.invoke(it)
         }
     }
 
-    private fun makeSettingsPopup(preference: Preference<Int>, offset: Int = 0, block: ((Int) -> Unit)? = null): PopupMenu {
-        return createPopupMenu { pos ->
-            preference.set(pos + offset)
-            block?.invoke(pos)
-        }
-    }
+    private fun setupPopupMenu(onItemClick: (Int) -> Unit) {
+        popup?.dismiss()
+        popup = PopupMenu(context, this, Gravity.END, androidx.appcompat.R.attr.actionOverflowMenuStyle, 0).apply {
+            entries.forEachIndexed { index, entry ->
+                menu.add(0, index, 0, entry).also { it.icon = emptyIcon }
+            }
+            (menu as? MenuBuilder)?.setOptionalIconsVisible(true)
+            menu[selectedPosition].icon = checkmarkIcon
 
-    private fun makeSettingsPopup(): PopupMenu {
-        return createPopupMenu { pos ->
-            onItemSelectedListener?.invoke(pos)
+            setOnMenuItemClickListener { menuItem ->
+                val pos = menuClicked(menuItem)
+                onItemClick(pos)
+                true
+            }
         }
+
+        setOnTouchListener(popup?.dragToOpenListener)
+        setOnClickListener { popup?.show() }
     }
 
     private fun menuClicked(menuItem: MenuItem): Int {
-        val pos = menuItem.itemId
-        setSelection(pos)
-        return pos
-    }
-
-    @SuppressLint("RestrictedApi")
-    fun createPopupMenu(onItemClick: (Int) -> Unit): PopupMenu {
-        val popup = PopupMenu(context, this, Gravity.END, androidx.appcompat.R.attr.actionOverflowMenuStyle, 0)
-        entries.forEachIndexed { index, entry ->
-            popup.menu.add(0, index, 0, entry)
-        }
-        (popup.menu as? MenuBuilder)?.setOptionalIconsVisible(true)
-        popup.menu.forEach {
-            it.icon = emptyIcon
-        }
-        popup.menu[selectedPosition].icon = checkmarkIcon
-        popup.setOnMenuItemClickListener { menuItem ->
-            val pos = menuClicked(menuItem)
-            onItemClick(pos)
-            true
-        }
-        return popup
+        menuItem.itemId.also(this::setSelection)
     }
 }
